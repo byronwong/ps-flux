106e1b2c1c61ceb2917e1ea2ace043489a42c010
Byron Wong <byronwong@byrons-mbp.home>
2021-01-18 14:54:06 +0000

    Completed Chapter 5 - Creating Home and about pages - basic routing non react-router-dom - created nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+import Header from './components/common/Header';
 
 function App() {
+  // Starting point for our app
+  // To make code readable, you create starting points for someone else to read
+
+  // As this is a function you can usn a function in a function
+  const getPage = () => {
+    const route = window.location.pathname;
+    if (route === '/about') return <AboutPage />;
+    return <HomePage />;
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      {getPage()}
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="jumbptron">
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps</p>
+    </div>
+  );
+};
+
+export default Homepage;
